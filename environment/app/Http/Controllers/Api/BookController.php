14511caf418e9a91bbb81b7340e613f9564b534b
kohei-iwamoto-wa <bananadesu9@gmail.com>
2021-05-18 22:59:12 +0900

    BookのCRUD作成

---
+++ environment/app/Http/Controllers/Api/BookController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+use App\Models\Book;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $books = Book::all();
+        return response()->json([
+            'message' => 'ok',
+            'data' => $books
+        ], 200, [], JSON_UNESCAPED_UNICODE);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $book = Book::create($request->all());
+        return response()->json([
+            'message' => 'Book created successfully',
+            'data' => $book
+        ], 201, [], JSON_UNESCAPED_UNICODE);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $update = [
+            'title' => $request->title,
+            'author' => $request->author
+        ];
+        Book::where('id', $id)->update($update);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Book::where('id', $id)->delete();
+    }
+}
